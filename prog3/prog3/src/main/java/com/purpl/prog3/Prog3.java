fb6df443c36b76c23090eaf101074bc411b767c8
Khara <egorrvanijked@gmail.com>
2026-05-07 21:17:53 +0300

    Lab3_full

---
+++ prog3/prog3/src/main/java/com/purpl/prog3/Prog3.java
@@ -0,0 +1,7 @@
+package com.purpl.prog4;
+
+public class Prog3 {
+    public static void main(String[] args) {
+        javax.swing.SwingUtilities.invokeLater(() -> new JFrameGUI().setVisible(true));
+    }
+}
