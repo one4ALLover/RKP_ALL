e8b4a7ee417edb742183e41bfec5d4217a04a6ab
Khara <egorrvanijked@gmail.com>
2026-05-07 21:24:53 +0300

    Lab5_full

---
+++ prog/src/main/java/com/purpl/prog5/Prog5.java
@@ -0,0 +1,7 @@
+package com.purpl.prog5;
+
+public class Prog5 {
+    public static void main(String[] args) {
+        javax.swing.SwingUtilities.invokeLater(() -> new JFrameGUI().setVisible(true));
+    }
+}
